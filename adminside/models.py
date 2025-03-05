62db5aec2d54e57860403e50f6b2a44bbfb804e1
Henil <henil00008@gmail.com>
2025-03-05 16:53:09 +0530

    save

--- adminside/models.py
+++ adminside/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -30,7 +29,6 @@ class Inventory(models.Model):
     cost_price=models.IntegerField(null=False)
     mfg_date=models.DateField()
     exp_date=models.DateField()
-=======
 from django.db import models, connection
 
 class Table(models.Model):
@@ -121,4 +119,3 @@ class Staff(models.Model):
   staff_role=models.CharField(max_length=50)
   branch=models.CharField(max_length=50)
 
->>>>>>> 39993bdcad5242a1df9c8f38a8311e932d43323e
